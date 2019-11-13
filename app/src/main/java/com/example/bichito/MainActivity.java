c856afd99b7eaf1f52afb7fe1730b55c3e46c189
unknown <Admin@IHR80PBF211.continental.edu.pe>
2019-11-12 22:23:49 -0500

    mi segundo cambio

--- app/src/main/java/com/example/bichito/MainActivity.java
+++ app/src/main/java/com/example/bichito/MainActivity.java
@@ -2,15 +2,37 @@ package com.example.bichito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnProducts;
+    private  Button btnAboutUs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //Enlazamos Activity + Layout
+        btnProducts = (Button)findViewById(R.id.btnEnter);
+        btnAboutUs = (Button) findViewById(R.id.btnAboutUs);
+        btnProducts.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void  onClick(View v){
+                //Log.i(Setting.INFO, "Products");
+                Intent i = new Intent( MainActivity.this, ProductListActivity.class);
+                startActivity(i);
+            }
+        });
+        btnAboutUs.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //Log.i(Setting.INFO, "About us");
+                Intent i = new Intent(MainActivity.this, AboutUsActivity.class);
+                startActivity(i);
+            }
+        });
     }
     @Override
     protected void onResume() {
